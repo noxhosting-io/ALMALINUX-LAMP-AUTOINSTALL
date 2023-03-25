58f97cc810bb63856c8e7ec4ded96d940ce93afc
KALIXHOSTING <93708739+KALIXHOSTING@users.noreply.github.com>
2023-03-25 02:53:03 -0500

    Update ovpn.sh

--- ovpn.sh
+++ ovpn.sh
@@ -32,7 +32,7 @@ elif [[ -e /etc/debian_version ]]; then
 	group_name="nogroup"
 elif [[ -e /etc/almalinux-release || -e /etc/rocky-release || -e /etc/orcale-release || -e /etc/centos-release ]]; then
 	os="centos"
-	os_version=$(grep -shoE '[0-9]+' /etc/almalinux-release /etc/rocky-release /etc/centos-release | head -1)
+	os_version=$(grep -shoE '[0-9]+' /etc/almalinux-release /etc/rocky-release /etc/orcale-release /etc/centos-release | head -1)
 	group_name="nobody"
 elif [[ -e /etc/fedora-release ]]; then
 	os="fedora"
